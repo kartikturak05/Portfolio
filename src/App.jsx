a9ca8f7739b86d05b007bb9a0de43ef811c46f54
Kartik Turak <kartikturak1@gmail.com>
2025-03-28 23:48:12 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+<link href="/src/styles.css" rel="stylesheet"></link>
+
+const App = () => {
+  return (
+    <h1 class="text-3xl font-bold underline">
+    Kartik Turak Portfolio
+  </h1>
+  )
+}
+
+export default App
